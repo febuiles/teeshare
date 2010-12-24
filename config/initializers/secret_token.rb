00e97a6134805b0386118ea4ebca01406f35961b
Federico Builes <federico.builes@gmail.com>
2010-12-24 04:00:17 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Threadless::Application.config.secret_token = '316ad2351ede1ad0c8798e354b6d433e52460cfd4918cd29db179ec140db67f3a0f0372c0d3d2ac0901a49191d7b3b6fc8ca41ff7dfd3b4f3a5c308da95c8e1d'
