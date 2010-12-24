13ba6190472c28b90a2e967354d1a16e7a0c4a42
Federico Builes <federico.builes@gmail.com>
2010-12-24 06:49:40 -0500

    Typo

--- app/models/tee.rb
+++ app/models/tee.rb
@@ -3,8 +3,8 @@ require 'open-uri'
 class Tee < ActiveRecord::Base
   def self.gather_data_from_url(url)
     doc = Nokogiri::HTML(open(url))
-    # there are usually 5 ".title" in the doc, push our luck and pick first.
 
+    # there are usually 5 ".title" in the doc, push our luck and pick first.
     title = doc.css("h1.title").first.content
 
     # take the first image, it's usually the design without a model
