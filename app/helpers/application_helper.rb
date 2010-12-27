4ad9f7658293f36ca5f801dc8724c190ff14e7ae
Federico Builes <federico.builes@gmail.com>
2010-12-27 18:50:03 -0500

    Adding jQuery quicksearch.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def link_to(*args, &block)
+    name, url, *rest = args
+    if url =~ /threadless\.com/
+      super(name, "#{url}?streetteam=febuiles", *rest)
+    else
+      super(*args, &block)
+    end
+  end
 end
