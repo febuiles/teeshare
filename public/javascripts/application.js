edbbe225958a6368168d2d6dfd614373e2eca030
Federico Builes <federico.builes@gmail.com>
2011-01-15 18:59:06 -0500

    Use var in JS files.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,18 @@
 jQuery(document).ready(function($){
     $(".suggested-shirt").mouseenter(function(e){
-        $this = $(this);
-        var imageURL = $("img", $this).attr("src");
-        if ($this.children("div.tooltip").length == 0) {
+        var _this = $(this);
+        var imageURL = $("img", _this).attr("src");
+        if (_this.children("div.tooltip").length == 0) {
             var div = $("<div class='tooltip'>");
             var img = $("<img src='" + imageURL +"' />");
             div.append(img);
-            $this.append(div);
+            _this.append(div);
         }
     });
     $(".suggested-shirt").mouseleave(function(e){
         $(this).children("div.tooltip").remove();
     });
-    
+
     $(".page_navigation a").live("click", function(){
       $("html, body").animate({ scrollTop: 0 }, "slow");
       return false;
